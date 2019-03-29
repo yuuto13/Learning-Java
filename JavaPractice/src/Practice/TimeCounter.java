ee7556520d3299e4adc3153efb593c1c88e4d0be
yuuto <yuutojurgen@gmail.com>
2019-03-29 11:03:44 +0800

    add main method

--- JavaPractice/src/Practice/TimeCounter.java
+++ JavaPractice/src/Practice/TimeCounter.java
@@ -15,7 +15,9 @@ import javax.swing.Timer;
 
 class TimeCounter {
 
-	public TimeCounter() {}
+	private int secondsLeft = 0;
+	private Timer timer = null;
+	
 	public TimeCounter(int timeInSeconds) {
 		secondsLeft = timeInSeconds;
 		timer = new Timer(1000, event -> {
@@ -29,6 +31,7 @@ class TimeCounter {
 				secondsLeft--;
 		});
 	}
+	
 	public void startCounting() {
 		if(timer != null) {
 			timer.start();
@@ -43,8 +46,37 @@ class TimeCounter {
 		return secondsLeft == 0;
 	}
 	
-	private int secondsLeft = 0;
-	private Timer timer = null;
+	public static void main(String[] args) {
+		//Test TimeCounter class
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please input a time (10h10m10s):");
+		String time = in.next();
+		time = time.trim();
+		int h, m, s;
+		h = m = s = 0;
+		if(time.contains("h")) {
+			h = Integer.parseInt(time.split("h")[0]);
+			if(time.split("h").length > 1) 
+				time = time.split("h")[1];
+		}
+		if(time.contains("m")) {
+			m = Integer.parseInt(time.split("m")[0]);
+			if(time.split("m").length > 1)
+				time = time.split("m")[1];
+		}
+		if(time.contains("s")) {
+			s = Integer.parseInt(time.replace("s", ""));
+		}
+		int timeInSeconds = 60*60*h + 60*m + s;
+		TimeCounter counter = new TimeCounter(timeInSeconds);
+		System.out.println("Count Down Starts!");
+		counter.startCounting();
+		JOptionPane.showMessageDialog(null, "Quit Counting?");
+		if(!counter.isOver()) {
+			System.out.println("I'M NOT DONE YOU IDIOT! NOOOOOOO!");
+		}
+		System.exit(0);
+	}
 }
 
 class TalkingClock {
