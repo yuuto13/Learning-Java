7f7cbd0f6ccb23f31a4c63f00209f0f8256c9154
yuuto <yuutojurgen@gmail.com>
2019-04-03 15:28:43 +0800

    small tweaks

--- JavaPractice/src/creatures/creatureTest.java
+++ JavaPractice/src/creatures/creatureTest.java
@@ -1,11 +1,12 @@
 package creatures;
 
 import java.lang.reflect.*;
+import creatures.Animal.AnimalType;
 
 public class creatureTest {
 	
 	public static void main(String[] args) {
-		Animal bobby = new Animal("Bobby");
+		Animal bobby = new Animal("Bobby", AnimalType.dog);
 		try {
 			Method animalGetName = Animal.class.getMethod("getName");
 			String animalName = (String)animalGetName.invoke(bobby);
