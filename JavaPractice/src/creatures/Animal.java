75c6368acbe680b56a3ba6521f2ed6965126e2d7
yuuto <yuutojurgen@gmail.com>
2019-04-03 15:28:05 +0800

    add enum AnimalType

--- JavaPractice/src/creatures/Animal.java
+++ JavaPractice/src/creatures/Animal.java
@@ -4,6 +4,21 @@ import java.util.*;
 
 public class Animal implements Comparable<Animal>
 {
+	public enum HealthState { healthy, ill }
+	public enum AnimalType {
+		undefined, cat, dog, horse, cow, chicken, duck, fish, pig, bird, fox
+	}
+	long id;
+	static long animalAmount = 0;
+	boolean hasOwner;
+	String name;
+	float weight;
+	float height;
+	byte age;
+	AnimalType type;
+	HealthState healthState;
+	
+	
 	public Animal() {
 		animalAmount++;
 		id = animalAmount;
@@ -12,26 +27,23 @@ public class Animal implements Comparable<Animal>
 		weight = 0.1f;
 		height = 0.1f;
 		age = 1;
+		type = AnimalType.undefined;
+		healthState = HealthState.healthy;
 	}
-	public Animal(String name) {
+	public Animal(String name, AnimalType type) {
 		this();
 		this.name = name;
+		this.type = type;
 	}
 
 	public String getName() { return name; }
 	public int compareTo(Animal other) {
 		return (int)(id - other.id);
 	}
+	public String toString() {
+		return name + " the " + type;
+	}
 	
-	private long id;
-	private static long animalAmount = 0;
-	private boolean hasOwner;
-	private String name;
-	private float weight;
-	private float height;
-	private byte age;
-	private enum HealthState{ healthy, ill }
-	//private HealthState healthState = HealthState.healthy;
 
 	
 	
